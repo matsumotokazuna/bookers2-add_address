f2b85a2c512429907d67203e74a78ce093631b01
vagrant <vagrant@localhost.localdomain>
2020-04-22 13:41:18 +0000

    [add]topic7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_073432) do
+ActiveRecord::Schema.define(version: 2020_04_22_131533) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 2020_04_19_073432) do
     t.datetime "updated_at", null: false
     t.text "introduction"
     t.string "profile_image_id"
+    t.string "postal_code"
+    t.string "prefecture_code"
+    t.string "city"
+    t.string "building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
